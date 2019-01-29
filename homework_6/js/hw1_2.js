a636f5171eaf042a531e02ee033c5b22bd68e51c
Cyxapic <truecyxapic@yandex.ru>
2019-01-29 17:30:28 +0300

    ADD HW6 1-2

--- homework_6/js/hw1_2.js
+++ homework_6/js/hw1_2.js
@@ -1,86 +1,60 @@
 'use strict';
 
-        /*
-         * Реализовать модуль корзины. У каждого товара есть кнопка «Купить», при нажатии на которую
-         * происходит добавление имени и цены товара в блок корзины. Корзина должна уметь считать
-         * общую сумму заказа. Один товар можно добавить несколько раз.
-         */
+/*
+ * Реализовать модуль корзины. У каждого товара есть кнопка «Купить», при нажатии на которую
+ * происходит добавление имени и цены товара в блок корзины. Корзина должна уметь считать
+ * общую сумму заказа. Один товар можно добавить несколько раз.
+ */
 
-        /**
-         * @property {object} settings Настройки корзины товаров.
-         * @property {{price: number, name: string}[]} goods Список товаров что купил пользователь.
-         * @property {HTMLElement} basketCountEl Место для показа количества товаров.
-         * @property {HTMLElement} basketPriceEl Место для показа цены всех товаров.
-         */
+/**
+ * @property {object} settings Настройки корзины товаров.
+ * @property {{price: number, name: string}[]} goods Список товаров что купил пользователь.
+ * @property {HTMLElement} basketCountEl Место для показа количества товаров.
+ * @property {HTMLElement} basketPriceEl Место для показа цены всех товаров.
+ */
 
-        class Basket {
-            constructor(userSettings = {}) {
-                this.settings = {
-                    container: '#container',
-                    countSelector: '#basket-count',
-                    priceSelector: '#basket-price',
-                };
-                this.goods = [];
-                this.countEl = null;
-                this.priceEl = null;
-                Object.assign(this.settings, userSettings);
-            };
-            /**
-             * Отображает количество всех товаров и их цену.
-             */
-            render() {
+class Basket {
+    constructor(userSettings = {}) {
+        this.settings = {
+            countSelector: '#basket-count',
+            priceSelector: '#basket-price',
+        };
+        Object.assign(this.settings, userSettings);
+        this.goods = [];
+        this.countEl = document.querySelector(this.settings.countSelector);
+        this.priceEl = document.querySelector(this.settings.priceSelector);
+    };
+    /**
+     * Отображает количество всех товаров и их цену.
+     */
+    render() {
+        console.log(this.goods);
+        console.log(this.countEl);
+        this.countEl.innerText = this.goods.length;
+        this.priceEl.innerText = this.getGoodsPrice();
+    };
+    /**
+     * Считает и возвращает цену всех купленных товаров из массива this.goods.
+     * @returns {number} Цену всех купленных товаров.
+     */
+    getGoodsPrice() {
+        let summ = 0;
+        this.goods.forEach(el => {
+            summ += +el.goodPrice;
+        });
+        console.log(summ);
+        return summ;
+    };
 
-            };
-            /**
-             * Считает и возвращает цену всех купленных товаров из массива this.goods.
-             * @returns {number} Цену всех купленных товаров.
-             */
-            getGoodsPrice() {};
-
-            /**
-             * Добавляет купленный товар в массив купленных товаров и отображает количество и цену всех товаров.
-             * @param goodName Название товара.
-             * @param goodPrice Цена товара.
-             */
-            add(goodName, goodPrice) {
-                const container = document.querySelectorAll(this.settings.container);
-                container.forEach((el) => {
-                    el.addEventListener('click', );
-                });
-            };
-
-            
-
-        }
-        // const basket = {
-        //     settings: {
-        //         countSelector: '#basket-count',
-        //         priceSelector: '#basket-price',
-        //     },
-        //     goods: [],
-        //     countEl: null,
-        //     priceEl: null,
-
-        //     /**
-        //      * Инициализирует переменные для корзины и показывает эти значения.
-        //      */
-        //     init(settings = {}) {},
-
-        //     /**
-        //      * Отображает количество всех товаров и их цену.
-        //      */
-        //     render() {},
-
-        //     /**
-        //      * Считает и возвращает цену всех купленных товаров из массива this.goods.
-        //      * @returns {number} Цену всех купленных товаров.
-        //      */
-        //     getGoodsPrice() {},
-
-        //     /**
-        //      * Добавляет купленный товар в массив купленных товаров и отображает количество и цену всех товаров.
-        //      * @param goodName Название товара.
-        //      * @param goodPrice Цена товара.
-        //      */
-        //     add(goodName, goodPrice) {},
-        // };
+    /**
+     * Добавляет купленный товар в массив купленных товаров и отображает количество и цену всех товаров.
+     * @param goodName Название товара.
+     * @param goodPrice Цена товара.
+     */
+    add(goodName, goodPrice) {
+        this.goods.push({
+            goodName: goodName,
+            goodPrice: goodPrice
+        });
+    };
+}
